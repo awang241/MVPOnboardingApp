17ddcda7d775c00d0caf23a76ce5461cc3fef5e8
Alan Wang <a.wang.24136@gmail.com>
2024-08-11 22:02:44 +1200

    Adding basic navbar to app

---
+++ mvpinternshipapp.client/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import "./Navbar.css";
+
+export default function Navbar() {
+    return (
+        <div className="navbar">
+            <a>Products</a>
+            <a>Customers</a>
+            <a>Stores</a>
+            <a>Sales</a>
+        </div>
+    );
+}   
